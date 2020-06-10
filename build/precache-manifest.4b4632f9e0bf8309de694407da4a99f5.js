ddea9cd6022d206e78276d53a4581098222665b2
sammy kirigha <dkirigha18@gmail.com>
2020-06-10 20:46:05 +0300

    new adds

---
+++ build/precache-manifest.4b4632f9e0bf8309de694407da4a99f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654a034a8b39666fd0e6b61eacba460a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "690c1cdb6b40f8cb317c",
+    "url": "/static/css/main.50b8bd80.chunk.css"
+  },
+  {
+    "revision": "440f9fff100e7e7739ab",
+    "url": "/static/js/2.1ab05e40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ab05e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690c1cdb6b40f8cb317c",
+    "url": "/static/js/main.d6d8c598.chunk.js"
+  },
+  {
+    "revision": "e2c2cf3b526dbe2a665b",
+    "url": "/static/js/runtime-main.4d0a4df8.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/static/media/room-2.052c7681.jpeg"
+  }
+]);
