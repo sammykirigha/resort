71e02ba96e62d3b96cce489b238057928f362386
sammy kirigha <dkirigha18@gmail.com>
2020-06-10 18:12:05 +0300

    new adds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
-// import './App.css';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import SingleRoom from './pages/SingleRoom';
+import  Rooms  from './pages/Rooms';
+import './App.css';
+import {Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-   <>hello from app</>
+   <>
+   <Navbar />
+   <Switch>
+   <Route exact path="/" component={Home} />
+   <Route exact path="/rooms" component={Rooms} />
+   <Route exact path="/rooms/:slug" component={SingleRoom} />
+   <Route component={Error} />
+   </Switch>
+   </>
+
   );
 }
 
